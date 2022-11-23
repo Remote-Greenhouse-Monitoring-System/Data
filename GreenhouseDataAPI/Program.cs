15e4178fb97dd593a56a91cd5564d9e0774555d5
Mishanull <mihaiavram72@gmail.com>
2022-11-23 01:23:24 +0100

    cors policy

--- GreenhouseDataAPI/Program.cs
+++ GreenhouseDataAPI/Program.cs
@@ -3,6 +3,7 @@ using EFCData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WebSocketClients.Clients;
 using WebSocketClients.Interfaces;
 
@@ -18,14 +19,23 @@ builder.Services.AddDbContext<GreenhouseContext>();
 builder.Services.AddScoped<IMeasurementService, MeasurementDAO>();
 builder.Services.AddScoped<IMeasurementClient, MeasurementClient>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(cbuilder =>
+    {
+        cbuilder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsProduction() || app.Environment.IsDevelopment())
+if ( app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
